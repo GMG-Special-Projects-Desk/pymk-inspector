26182d875177e6370917d533021e33b190f54edb
surya <surya@suryamattu.com>
2017-09-08 13:17:16 -0400

    back to menubar with working chrome for scrape

--- src/main/index.js
+++ src/main/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
-// import { app, BrowserWindow } from 'electron'
-import { app } from 'electron'
+import { testing } from '../renderer/scripts/scrape'
+import { app, ipcMain } from 'electron'
 import menubar from 'menubar'
 /**
  * Set `__static` path to static files in production
@@ -11,11 +11,42 @@ if (process.env.NODE_ENV !== 'development') {
   global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
 let mb
-// let mainWindow
 const winURL = process.env.NODE_ENV === 'development'
   ? `http://localhost:9080`
   : `file://${__dirname}/index.html`
 
+ipcMain.on('async', (event, arg) => {
+  testing(event)
+  console.log(arg)
+})
+function createMenuBar () {
+  mb = menubar({icon: require('path').join(__static, 'facebook-inspector.png'),
+    index: winURL,
+    alwaysOnTop: true
+  })
+  mb.on('ready', function () {
+    console.log('app is ready')
+  })
+  // mb.on('after-create-window', function () {
+  //   mb.window.webContents.openDevTools()
+  // })
+}
+
+app.on('ready', createMenuBar)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mb === null) {
+    createMenuBar()
+  }
+})
+
+// import { app, BrowserWindow, ipcMain } from 'electron'
+// let mainWindow
 // function createWindow () {
 //   /**
 //    * Initial window options
@@ -28,38 +59,11 @@ const winURL = process.env.NODE_ENV === 'development'
 
 //   mainWindow.loadURL(winURL)
 //   mainWindow.openDevTools()
-
 //   mainWindow.on('closed', () => {
 //     mainWindow = null
 //   })
 // }
-
-function createMenuBar () {
-  mb = menubar({icon: require('path').join(__static, 'facebook-inspector.png'),
-    index: winURL,
-    alwaysOnTop: true
-  })
-  mb.on('ready', function () {
-    console.log('app is ready')
-  })
-  mb.on('after-create-window', function () {
-    mb.window.webContents.openDevTools()
-  })
-}
-
-app.on('ready', createMenuBar)
 // app.on('ready', createWindow)
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  if (mb === null) {
-  // if (mainWindow === null) {
-    // createWindow()
-    createMenuBar()
-  }
-})
+// if (mainWindow === null) {
+// createWindow()
+// }
